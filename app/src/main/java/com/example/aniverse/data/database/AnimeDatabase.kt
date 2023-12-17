42fdbadbd501d48b11f93ea01890f8cdf12a77b8
Jesushs4 <lagt5799@gmail.com>
2023-12-17 18:44:57 +0100

    v1.0

--- app/src/main/java/com/example/aniverse/data/database/AnimeDatabase.kt
+++ app/src/main/java/com/example/aniverse/data/database/AnimeDatabase.kt
@@ -26,7 +26,7 @@ abstract class AnimeDatabase : RoomDatabase() {
         return Room.databaseBuilder(
             context.applicationContext,
             AnimeDatabase::class.java,
-            "anime12_db"
+            "aniverse_db"
         ).build()
 
     }
